3286383f131fd49aef720ef08b1a4aa61e8ed418
Pavla Beránková <berankovap@protonmail.com>
2023-11-30 19:33:12 +0100

    create a new folder by Year

---
+++ 2022/AoC2022_Day03.playground/Contents.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+var lines = [String]()
+
+if let filepath = Bundle.main.path(forResource: "input03", ofType: "txt") {
+    do {
+        var data = try String(contentsOfFile: filepath).components(separatedBy: "\n").filter { !$0.isEmpty }
+            lines = data
+    } catch {
+         print("File not loaded")
+    }
+}
+
+let charIndex = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+func part1() -> String {
+    var sumPriority = 0
+
+    for line in lines {
+        let halfLength = line.count / 2
+
+        let firstHalf = line.prefix(halfLength)
+        let secondHalf = line.suffix(halfLength)
+
+        var firstRucksack = [String]()
+        var secondRucksack = [String]()
+
+        for char1 in firstHalf {
+            firstRucksack.append(String(char1))
+        }
+
+        for char2 in secondHalf {
+            secondRucksack.append(String(char2))
+        }
+
+        var result = Set(firstRucksack).intersection(Set(secondRucksack))
+
+        var itemPriority = (charIndex.firstIndex(of: result.first!))!
+        sumPriority += itemPriority + 1
+    }
+    return "Result of part one is \(sumPriority)."
+}
+
+func part2() -> String {
+    var sumPriority = 0
+    var partOfGroup = ""
+    var elfGroup = [String]()
+
+    for i in stride(from: 0, to: lines.count - 1, by: 3) {
+        for j in i..<(i + 3) {
+            partOfGroup += lines[j]
+            elfGroup.append(partOfGroup)
+            partOfGroup = ""
+        }
+        var item = Set(elfGroup[0]).intersection(Set(elfGroup[1])).intersection(Set(elfGroup[2]))
+
+        var itemPriority = charIndex.firstIndex(of: String(item.first!))!
+        sumPriority += itemPriority + 1
+        elfGroup = [String]()
+    }
+
+    return "Result of part two is \(sumPriority)."
+}
+
+part1()
+part2()
---
+++ 2022/AoC2022_Day04.playground/Contents.swift
@@ -0,0 +1,83 @@
+import Foundation
+
+var lines = [String]()
+
+if let filepath = Bundle.main.path(forResource: "input04", ofType: "txt") {
+    do {
+        var data = try String(contentsOfFile: filepath).components(separatedBy: "\n").compactMap { $0.isEmpty ? nil : $0 }
+            lines = data
+    } catch {
+         print("File not loaded")
+    }
+}
+
+func part1() -> String {
+    var result = 0
+    for line in lines {
+        let separateNumbers = line.components(separatedBy: CharacterSet(charactersIn: "-,"))
+
+        var firstElfRange = [Int]()
+        var secondElfRange = [Int]()
+
+        firstElfRange.append(Int(separateNumbers[0])!)
+        firstElfRange.append(Int(separateNumbers[1])!)
+
+        secondElfRange.append(Int(separateNumbers[2])!)
+        secondElfRange.append(Int(separateNumbers[3])!)
+
+        var firstElfSections = [Int]()
+        var secondElfSections = [Int]()
+
+        for i in (firstElfRange.first!...firstElfRange.last!) {
+            firstElfSections.append(i)
+        }
+
+        for i in (secondElfRange.first!...secondElfRange.last!) {
+            secondElfSections.append(i)
+        }
+
+        if (Set(firstElfSections).isSubset(of: Set(secondElfSections))) ||  (Set(secondElfSections).isSubset(of: Set(firstElfSections))) {
+            result += 1
+        }
+    }
+    return("Result of part 1 is \(result).")
+}
+
+func part2() -> String {
+    var result = 0
+    for line in lines {
+        let separateNumbers = line.components(separatedBy: CharacterSet(charactersIn: "-,"))
+
+        var firstElfRange = [Int]()
+        var secondElfRange = [Int]()
+
+        firstElfRange.append(Int(separateNumbers[0])!)
+        firstElfRange.append(Int(separateNumbers[1])!)
+
+        secondElfRange.append(Int(separateNumbers[2])!)
+        secondElfRange.append(Int(separateNumbers[3])!)
+
+        var firstElfSections = [Int]()
+        var secondElfSections = [Int]()
+
+        for i in (firstElfRange.first!...firstElfRange.last!) {
+            firstElfSections.append(i)
+        }
+
+        for i in (secondElfRange.first!...secondElfRange.last!) {
+            secondElfSections.append(i)
+        }
+
+        let bothElfSection = Set(firstElfSections).intersection(Set(secondElfSections))
+        if !bothElfSection.isEmpty {
+            result += 1
+        }
+            
+    }
+    return("Result of part 2 is \(result).")
+}
+
+part1()
+part2()
+
+
